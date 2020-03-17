3f5d755c1f32ad19e4ca015ed5f53dc2bfcbffd9
taoofshawn <29876809+taoofshawn@users.noreply.github.com>
2020-03-16 23:25:31 -0400

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hoverDdns
 Keep a hover DNS record up to date with your external IP
+
+
+| | |
+| -- | -- |
+| Info | Based on [hover-dns-updater.](https://github.com/texasaggie97/hover-dns-updater) Simplified and targetted for a simple docker image
---
+++ hoverDdns.py
@@ -0,0 +1,124 @@
+import json, sys, datetime, logging, os, time
+import requests
+
+
+class HoverClient:
+	def __init__(self, config):
+		self.username = config['HOVERUSER']
+		self.password = config['HOVERPASS']
+		self.getAuth()
+		self.hoverId = config['HOVERID']
+		self.hoverIp = self.getCurrentHoverIp()
+		self.currentIp = self.getCurrentExternalIp()
+
+	def getAuth(self):
+		logging.info('attempting authentication with Hover')
+		data = json.dumps({ "username": self.username, "password": self.password })
+		loginUrl = 'https://www.hover.com/api/login'
+		headers = {'Content-type': 'application/json'}
+
+		r = requests.post(loginUrl, data=data, headers=headers)
+		if not r.ok or "hoverauth" not in r.cookies:
+			logging.error('could not authenticate with Hover')
+			sys.exit(1)
+		logging.error('authenticated successfully with Hover')
+
+		self.hoverToken = {"hoverauth": r.cookies["hoverauth"]}
+		self.hoverTokenTimestamp = datetime.datetime.now()
+
+		#temp cache token in file ------------------------------------------------------
+		# try:
+		# 	os.remove(self.tokenFile)
+		# except:
+		# 	logging.info('token file was not deleted')
+		# else:
+		# 	logging.info('token file was deleted')
+		# with open(self.tokenFile, 'w', encoding='utf-8') as f:
+		# 	json.dump(self.hoverToken, f, ensure_ascii=False, indent=4, sort_keys=True)
+		#temp cache token in file ------------------------------------------------------
+
+	def checkAuth(self):
+		lastAuthDelta = datetime.datetime.now() - self.hoverTokenTimestamp
+		if lastAuthDelta.total_seconds() > (6 * 60 * 60): #ReAuth after 6 Hours
+			logging.info('reauthentication needed')
+			self.getAuth()
+
+		#temp
+		# logging.info('reading token from file')
+		# with open(self.tokenFile, 'r', encoding='utf-8') as f:
+		# 	self.hoverToken = json.load(f)
+		# logging.info('token read as {}'.format(self.hoverToken))
+		#temp
+
+	def call(self, method, resource, data=None):
+		self.checkAuth()
+		url = "https://www.hover.com/api/{0}".format(resource)
+		logging.info('connecting to Hover')
+		r = requests.request(method, url, data=data, cookies=self.hoverToken)
+		if not r.ok:
+			logging.error('could not connect to Hover!')
+		if r.content:
+			body = r.json()
+			if "succeeded" not in body or body["succeeded"] is not True:
+				logging.error('connected to Hover but data is missing!')
+			logging.info('request to Hover was successful')
+			return body
+
+	def getCurrentHoverIp(self):
+		logging.info('checking current IP address setting at Hover')
+		allHoverData = self.call("get", "dns")
+		for domain in allHoverData.get('domains'):
+			for entry in domain['entries']:
+				if entry['id'] == self.hoverId:
+					return entry['content']
+		return 'hoverConnectFailed'
+
+	def getCurrentExternalIp(self):
+		logging.info('checking current external IP address')
+		return requests.get('https://api.ipify.org').text
+
+	def updateHoverIp(self, newIp):
+		logging.info('updating hover IP with {}'.format(newIp))
+		return self.call('put', 'dns/' + self.hoverId, {'content': newIp})
+
+
+
+if __name__ == "__main__":
+
+	config = {
+		'HOVERUSER': os.getenv('HOVERUSER'),
+		'HOVERPASS': os.getenv('HOVERPASS'),
+		'HOVERID': os.getenv('HOVERID'),
+		'POLLTIME': os.getenv('HOVERPOLLTIME')
+	}
+
+	for key in config:
+		if key == 'POLLTIME':
+			try:
+				config[key] = int(config[key])
+			except:
+				config[key] = 360
+		elif config[key] is None:
+			logging.error('missing environment variable: {}'.format(key))
+			sys.exit(1)
+
+	logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+	client = HoverClient(config)
+
+	while True:
+		if client.hoverIp != client.currentIp:
+			logging.info(
+				'hover IP needs to be updated. Hover: {}, Actual: {}'.format(
+					client.hoverIp, client.currentIp))
+			try:
+				client.updateHoverIp(client.currentIp)
+			except:
+				logging.error('hover update failed')
+		else:
+			logging.info(
+				'hover IP does not need to be updated. Hover: {}, Actual: {}'.format(
+					client.hoverIp, client.currentIp))
+		logging.info('sleeping for {} minutes'.format(config['POLLTIME']))
+		time.sleep(config['POLLTIME']*60) #Change to seconds
+		client.hoverIp = client.getCurrentHoverIp()
+		client.currentIp = client.getCurrentExternalIp()
